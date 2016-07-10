cecbec24ef9a9dbcd38b31834de008d6edbfb7b0
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-07-10 13:31:13 -0300

    Inserted settings menu in movies

--- app/src/main/java/br/com/adalbertofjr/popularmovies/ui/fragments/MoviesFragment.java
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/fragments/MoviesFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,10 +24,22 @@ public class MoviesFragment extends Fragment {
     public MoviesFragment() {
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_movies_fragment, menu);
     }
 }
