3321acb085acbea95e47fcd186251b903af8ca1a
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-07-10 13:25:49 -0300

    Create MoviesFragment

--- app/src/main/java/br/com/adalbertofjr/popularmovies/ui/MainActivity.java
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.adalbertofjr.popularmovies;
+package br.com.adalbertofjr.popularmovies.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.adalbertofjr.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/fragments/MoviesFragment.java
@@ -0,0 +1,31 @@
+package br.com.adalbertofjr.popularmovies.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.adalbertofjr.popularmovies.R;
+
+/**
+ * Popular Movies
+ * MoviesFragment
+ * <p/>
+ * Created by Adalberto Fernandes Júnior on 10/07/2016.
+ * Copyright © 2016 - Adalberto Fernandes Júnior. All rights reserved.
+ */
+
+public class MoviesFragment extends Fragment {
+
+    public MoviesFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+        return rootView;
+    }
+}
