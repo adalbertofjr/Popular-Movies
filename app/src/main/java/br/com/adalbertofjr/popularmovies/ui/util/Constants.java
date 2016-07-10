074a1ea696acba6553dac413c4e2a2f689baf867
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-07-10 18:40:06 -0300

    Configured api key

---
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/util/Constants.java
@@ -0,0 +1,17 @@
+package br.com.adalbertofjr.popularmovies.ui.util;
+
+import br.com.adalbertofjr.popularmovies.BuildConfig;
+
+/**
+ * Popular Movies
+ * Constants
+ * <p/>
+ * Created by Adalberto Fernandes Júnior on 10/07/2016.
+ * Copyright © 2016 - Adalberto Fernandes Júnior. All rights reserved.
+ */
+
+public class Constants {
+
+    //KEY FOR API
+    public static String THE_MOVIE_DB_API_KEY = BuildConfig.THE_MOVIE_DB_KEY;
+}
