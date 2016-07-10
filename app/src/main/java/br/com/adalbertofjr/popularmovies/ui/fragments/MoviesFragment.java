33a5b37bb2b3714746798fd35d2138223853ba0a
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-07-10 19:20:01 -0300

    Implemented FetchMoviesTask

--- app/src/main/java/br/com/adalbertofjr/popularmovies/ui/fragments/MoviesFragment.java
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/fragments/MoviesFragment.java
@@ -1,9 +1,12 @@
 package br.com.adalbertofjr.popularmovies.ui.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,15 +16,27 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import br.com.adalbertofjr.popularmovies.R;
 import br.com.adalbertofjr.popularmovies.ui.DetailActivity;
 import br.com.adalbertofjr.popularmovies.ui.SettingsActivity;
 import br.com.adalbertofjr.popularmovies.ui.adapters.MoviesImageAdapter;
+import br.com.adalbertofjr.popularmovies.ui.util.Constants;
 
 /**
  * Popular Movies
  * MoviesFragment
- * <p>
+ * <p/>
  * Created by Adalberto Fernandes Júnior on 10/07/2016.
  * Copyright © 2016 - Adalberto Fernandes Júnior. All rights reserved.
  */
@@ -72,6 +87,109 @@ public class MoviesFragment extends Fragment {
             return true;
         }
 
+        if (id == R.id.action_refresh) {
+            FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
+            fetchMoviesTask.execute();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private class FetchMoviesTask extends AsyncTask<Void, Void, String[]> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(Void... voids) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String moviesJsonString = null;
+
+            try {
+                String apiKey = Constants.THE_MOVIE_DB_API_KEY;
+                Uri.Builder uri =
+                        Uri.parse("http://api.themoviedb.org/3/movie/popular?api_key=" + apiKey)
+                                .buildUpon();
+
+                URL url = new URL(uri.build().toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                moviesJsonString = buffer.toString();
+                Log.v(LOG_TAG, "Movie JSON String: " + moviesJsonString);
+
+                try {
+                    return getMoviesDataFromJson(moviesJsonString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+
+
+    private String[] getMoviesDataFromJson(String moviesJsonString)
+            throws JSONException {
+
+        //Json objects names
+        final String OWM_LIST = "results";
+        final String OWN_BACKGROUND = "backdrop_path";
+        final String OWN_POSTER = "poster_path";
+        final String OWN_VOTE_AVERAGE = "vote_average";
+        final String OWN_TITLE = "original_title";
+        final String OWN_RELEASE_DATE = "release_date";
+        final String OWN_OVERVIEW = "overview";
+
+        JSONObject moviesJson = new JSONObject(moviesJsonString);
+        JSONArray moviesArray = moviesJson.getJSONArray(OWM_LIST);
+
+
+        return new String[0];
     }
 }
