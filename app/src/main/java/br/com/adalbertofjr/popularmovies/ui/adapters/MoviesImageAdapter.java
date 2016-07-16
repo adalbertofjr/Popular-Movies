8f5be7b56da5619492201351f0655f1bcc6b414c
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-07-16 15:19:10 -0300

    Usando Picasso para carregar as imagens

--- app/src/main/java/br/com/adalbertofjr/popularmovies/ui/adapters/MoviesImageAdapter.java
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/adapters/MoviesImageAdapter.java
@@ -1,15 +1,17 @@
 package br.com.adalbertofjr.popularmovies.ui.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
-import br.com.adalbertofjr.popularmovies.R;
 import br.com.adalbertofjr.popularmovies.model.Movies;
 
 /**
@@ -27,34 +29,23 @@ public class MoviesImageAdapter extends ArrayAdapter<Movies> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Movies movie = getItem(position);
         ImageView imageView;
 
         if (convertView == null) {
             imageView = new ImageView(getContext());
             imageView.setLayoutParams(new GridView.LayoutParams(200, 360));
             imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            //imageView.setPadding(8, 8, 8, 8);
         } else {
             imageView = (ImageView) convertView;
         }
 
-        imageView.setImageResource(mThumbIds[0]);
-
+        String MOVIE_IMAGE_URL = "http://image.tmdb.org/t/p/w185";
+        Uri.Builder uriImage = Uri.parse(MOVIE_IMAGE_URL)
+                .buildUpon()
+                .appendEncodedPath(movie.getPoster_path());
+        String urlPoster = uriImage.build().toString();
+        Picasso.with(getContext()).load(urlPoster).into(imageView);
         return imageView;
     }
-
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-            R.mipmap.ic_launcher, R.mipmap.ic_launcher
-    };
 }
