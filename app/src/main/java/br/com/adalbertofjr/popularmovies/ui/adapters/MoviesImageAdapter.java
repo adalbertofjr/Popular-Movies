da6c41be0054fa04f20dabe87433cf5114a032e2
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-07-24 12:50:28 -0300

    Ajustado layout para lista de filmes

--- app/src/main/java/br/com/adalbertofjr/popularmovies/ui/adapters/MoviesImageAdapter.java
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/ui/adapters/MoviesImageAdapter.java
@@ -2,16 +2,17 @@ package br.com.adalbertofjr.popularmovies.ui.adapters;
 
 import android.content.Context;
 import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import br.com.adalbertofjr.popularmovies.R;
 import br.com.adalbertofjr.popularmovies.model.Movies;
 import br.com.adalbertofjr.popularmovies.util.Constants;
 
@@ -31,21 +32,16 @@ public class MoviesImageAdapter extends ArrayAdapter<Movies> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Movies movie = getItem(position);
-        ImageView imageView;
 
-        if (convertView == null) {
-            imageView = new ImageView(getContext());
-            imageView.setLayoutParams(new GridView.LayoutParams(200, 360));
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        } else {
-            imageView = (ImageView) convertView;
-        }
+        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.movies_item, parent, false);
 
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.iv_movies_item_poster);
         Uri.Builder uriImage = Uri.parse(Constants.MOVIE_IMAGE_POSTER_URL)
                 .buildUpon()
                 .appendEncodedPath(movie.getPoster_path());
         String urlPoster = uriImage.build().toString();
         Picasso.with(getContext()).load(urlPoster).into(imageView);
-        return imageView;
+
+        return rootView;
     }
 }
