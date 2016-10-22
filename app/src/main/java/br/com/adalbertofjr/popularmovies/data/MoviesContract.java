16570f2cce3d546e0587a539628a2c8dd0a1dff6
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2016-10-22 15:54:48 -0200

    Implementado cursor loader em detail para top_rated

--- app/src/main/java/br/com/adalbertofjr/popularmovies/data/MoviesContract.java
+++ app/src/main/java/br/com/adalbertofjr/popularmovies/data/MoviesContract.java
@@ -66,6 +66,9 @@ public class MoviesContract {
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TOP_RATED;
 
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TOP_RATED;
+
         // Top Rated table db
         public static final String TABLE_NAME = "top_rated";
         public static final String COLUMN_BACKDROP_PATH = "backdrop_path";
